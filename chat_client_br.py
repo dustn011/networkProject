3a03df589f852525425d48e5ef4e9b7edec4b12a
rabozzang <rabozzang@naver.com>
2023-02-02 09:27:13 +0900

    02.02 팀채팅방

--- chat_client_br.py
+++ chat_client_br.py
@@ -1,102 +1,209 @@
-from socket import *
-from threading import *
+# TCP 멀티 채팅 클라이언트 프로그램
+
+import json
 import sys
+import time
+
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
-import json
-import time
-from datetime import datetime
+from datetime import timedelta, datetime
+from socket import *
+from threading import *
 
-from_class = uic.loadUiType("ui/testtest.ui")[0]
+form_class = uic.loadUiType("ui/chat.ui")[0]
 
-class WindowClass(QWidget, from_class):
+
+class Client(QWidget, form_class):
     client_socket = None
 
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.initialize_socket()
-        self.test_thread()
-        self.set_chattingLog = 0
+        # Ui 페이지 0번째 페이지로 고정
         self.stackedWidget.setCurrentIndex(0)
-        # 입장하기 버튼 클릭시 메인페이지로 이동
-        self.start_btn.clicked.connect(self.mainhome)
-        # 전체체팅방 전송버튼 클릭시 메세지 송출
-        self.btn_send.clicked.connect(self.send_chat)
 
+        # 이름 입력하고 채팅방 입장하기
+        self.led_insertName.returnPressed.connect(self.method_moveChattingPage)
+        self.btn_joinChat.clicked.connect(self.method_moveChattingPage)
+
+        # 채팅방 나가기
+        self.btn_leaveChat.clicked.connect(self.method_leaveChattingRoom)
+
+        # 메세지 보내기
+        self.btn_sendMessage.clicked.connect(self.method_sendMessage)
+        self.led_sendMessage.returnPressed.connect(self.method_sendMessage)
+
+        # 소켓 만들기
+        self.initialize_socket()
+
+        # 스레드 함수 실행
+        self.listen_thread()
+        self.set_chattingLog = 0
+
+        # 채팅방 만들기
+        self.btn_insertTeamChat.clicked.connect(self.newchat)
+        self.listwdg_teamChat.itemClicked.connect(self.newchatroom)
+        self.btn_leaveTeamChat.clicked.connect(self.outchat)
+        self.btn_teamChatSendMessage.clicked.connect(self.newSend)
+
+
+    # 소켓 설정 메서드
     def initialize_socket(self):
-        ip = input('접속할 ip를 입력해주세요 : ')
+        ip = input("서버 IP를 입력해주세요(default=10.10.21.102): ")
         if ip == '':
-            ip = '127.0.0.1'
-        port = 5959
+            ip = '10.10.21.118'
+        port = 6666
+
+        # TCP socket을 생성하고 server와 연결
         self.client_socket = socket(AF_INET, SOCK_STREAM)
         self.client_socket.connect((ip, port))
 
-    def send_chat(self):
-        sender_name = self.nameline.text()
-        message = self.sendmessage.text()
+    # 채팅방 입장하기 메서드
+    def method_moveChattingPage(self):
+        if not bool(self.led_insertName.text()):
+            QMessageBox.information(self, '입력 오류', '이름을 입력해주세요')
+        else:
+            self.lbl_wellcome.setText(f'{self.led_insertName.text()}님 환영합니다')
+            self.listwdg_connectionPeople.addItem(self.led_insertName.text())
+            self.listwdg_chattingBox.addItem(f"\n<<< [{datetime.now().strftime('%D %T')}] [{self.led_insertName.text()}] 님이 채팅방에 입장하셨습니다 >>> \n")
+            # 리스트 위젯 스크롤바 아래로 고정
+            self.listwdg_chattingBox.scrollToBottom()
+            self.listwdg_connectionPeople.scrollToBottom()
+
+            # 알람 서버로 전송
+            alarm = ['plzReceiveAlarm', datetime.now().strftime('%D %T'), self.led_insertName.text()]     # 인덱스 0번에 식별자 'alarm'넣어줌
+            send_alarm = json.dumps(alarm)
+            self.client_socket.send(send_alarm.encode('utf-8'))
+            self.stackedWidget.setCurrentIndex(1)
+
+
+
+    # 채팅방 나가기 메서드
+    def method_leaveChattingRoom(self):
+        self.led_insertName.clear()
+        self.stackedWidget.setCurrentIndex(0)
+
+    # 메시지를 받는 메서드 스레드로 실행
+    def listen_thread(self):
+        # 데이터 수신 thread를 생성하고 시작
+        t = Thread(target=self.receive_message, args=(self.client_socket,), daemon=True)
+        t.start()
+
+    # 스레드에서 실행되는 메시지 받기. 메서드 message_log[0]번으로 식별자 구분
+    def receive_message(self, so):
+        while True:
+            buf = so.recv(9999)
+            message_log = json.loads(buf.decode('utf-8'))
+            print(message_log)
+            time.sleep(0.1)
+            if not buf:     # 연결이 종료됨
+                break
+            # 처음 입장했을 때 모든 채팅 내역 출력
+            elif message_log[0] == 'allChat_data':
+                message_log.pop(0)
+                a = 1
+                setting = ''
+                for i in range(len(message_log)):
+                    if a%3 != 0:
+                        setting += f"[{message_log[i]}] "
+                    else:
+                        self.listwdg_chattingBox.addItem(setting + '\n' + message_log[i] + '')
+                        setting = ''
+                        # 리스트 위젯 스크롤바 아래로 고정
+                        self.listwdg_chattingBox.scrollToBottom()
+                    a += 1
+            # 처음 입장했을 때 현재 접속 인원 출력
+            elif message_log[0] == 'allConnection_data':
+                message_log.pop(0)
+                for i in range(len(message_log)):
+                    self.listwdg_connectionPeople.addItem(message_log[i])
+                    self.listwdg_connectionPeople.scrollToBottom()
+            # 다른 클라이언트에서 보낸 메세지 전체 메시지창에 출력
+            elif message_log[0] == 'plzReceiveMessage':
+                message_log.pop(0)
+                self.listwdg_chattingBox.addItem(message_log[0])
+                # 리스트 위젯 스크롤바 아래로 고정
+                self.listwdg_chattingBox.scrollToBottom()
+
+            # 다른 클라이언트에서 입장한 알림 전체 메시지창에 출력, 입장한 사람들 리스트에 넣어주기
+            elif message_log[0] == 'plzReceiveAlarm':
+                message_log.pop(0)
+                self.listwdg_chattingBox.addItem(message_log[0])
+                self.listwdg_connectionPeople.addItem(message_log[1])
+                # 리스트 위젯 스크롤바 아래로 고정
+                self.listwdg_chattingBox.scrollToBottom()
+                self.listwdg_connectionPeople.scrollToBottom()
+
+            # 다른 클라이언트 채팅방목록 보여주기
+            elif message_log[0] == 'plzReceiveNewchat':
+                message_log.pop(0)
+                for i in range(len(message_log)):
+                    self.listwdg_teamChat.addItem(message_log[i])
+                    self.listwdg_teamChat.scrollToBottom()
+
+        so.close()
+
+    # 메시지 보내기 메서드
+    def method_sendMessage(self):
+        sender_name = self.led_insertName.text()
+        message = self.led_sendMessage.text()
         message_datetime = datetime.now().strftime("%D %T")
 
         # 시간, 이름, 메시지 내용 순으로 리스트에 저장
-        send_messageList = [message_datetime, sender_name, message]
+        send_messageList = ['plzReceiveMessage', message_datetime, sender_name, message]
         setMessageData = json.dumps(send_messageList)  # json.dumps로 리스트의 값들 바이트형으로 바꿔줌
         self.client_socket.send(setMessageData.encode('utf-8'))  # 연결된 소켓(서버)에 채팅 로그 데이터 보내줌
 
         # 리스트 위젯에 작성한 글 append해줌
-        self.receivemessage.addItem(f"[{sender_name}][{message_datetime}]\n{message}\n")
-        self.sendmessage.clear()  # 작성한 글은 전송 후 ui에서 지워줌
+        self.listwdg_chattingBox.addItem(f"[{message_datetime}] [{sender_name}]\n{message}")
+        self.led_sendMessage.clear()    # 작성한 글은 전송 후 ui에서 지워줌
+
         # 리스트 위젯 스크롤바 아래로 고정
-        self.receivemessage.scrollToBottom()
+        self.listwdg_chattingBox.scrollToBottom()
 
-        # data = (f"{senders_name} : {message}").encode('utf-8')
-        # self.receivemessage.addItem(f"[{senders_name}]\n{message}\n")
-        # print(self.client_socket)
-        # # print(data)
-        # self.client_socket.send(data)
-        # self.sendmessage.clear()
-        # # print(senders_name)
+    def newchat(self):
+        # 새로운 채팅방 이름 설정
+        chatname = self.led_insertTeamChat.text()
+        # 채팅방이름을 리스트에 저장
+        send_newchatlist = ['plzReceiveNewchat',chatname]
+        # json.dumps로 리스트의 값들 바이트형으로 바꿔줌
+        send_newchat = json.dumps(send_newchatlist)
+        # 연결된 소켓(서버)에 채팅 로그 데이터 보내줌
+        self.client_socket.send(send_newchat.encode('utf-8'))
+        # 새채팅방을 채팅방리스트에 append해줌
+        self.listwdg_teamChat.addItem(chatname)
+        # 채팅방이름은 ui에서 지워줌
+        self.led_insertTeamChat.clear()
 
-    def mainhome(self):
-        if not bool(self.nameline.text()):
-            QMessageBox.information(self, '입력 오류', '이름을 입력해주세요')
-        else:
-            self.user_name.setText(f'{self.nameline.text()}님 환영합니다')
-            self.stackedWidget.setCurrentIndex(1)
+    def newchatroom(self):
+        # 신규채팅방으로 이동
+        self.stcwdg_teamchatting.setCurrentIndex(1)
+        self.label_teamChatName.setText(self.listwdg_teamChat.currentItem().text())
 
-    def test_thread(self):
-        cThread = Thread(target=self.receive_message, args=(self.client_socket,), daemon=True)
-        cThread.start()
+    def outchat(self):
+        self.stcwdg_teamchatting.setCurrentIndex(0)
 
-    def receive_message(self, so):
-        while True:
-            buf = so.recv(2560)
-            if not buf:
-                break
-            elif self.set_chattingLog == 0:
-                message_log = json.loads(buf.decode('utf-8'))
-                a = 1
-                setting = ''
-                for i in range(len(message_log)):
-                    if a % 3 != 0:
-                        setting += f"[{message_log[i]}] "
-                    else:
-                        self.receivemessage.addItem(setting + '\n' + message_log[i] + '\n')
-                        setting = ''
-                a += 1
-                self.set_chattingLog += 1
-            else:
-                self.receivemessage.addItem(buf.decode() + '\n')
-                # 리스트 위젯 스크롤바 아래로 고정
-            self.receivemessage.scrollToBottom()
-            time.sleep(0.1)
-            so.close()
+    def newSend(self):
+        sender_name = self.led_insertName.text()
+        message = self.led_teamChatSendMessage.text()
+        roomname = self.label_teamChatName.text()
+        message_datetime = datetime.now().strftime("%D %T")
 
-        #self.receivemessage.append(buf.decode() + '\n')
-        # so.close()
+        # 시간, 이름, 메시지 내용 순으로 리스트에 저장
+        newsend_messageList = ['plzReceiveNewMessage',roomname, sender_name, message]
+        setNewMessageData = json.dumps(newsend_messageList)  # json.dumps로 리스트의 값들 바이트형으로 바꿔줌
+        self.client_socket.send(setNewMessageData.encode('utf-8'))  # 연결된 소켓(서버)에 채팅 로그 데이터 보내줌
+
+        # 리스트 위젯에 작성한 글 append해줌
+        self.listwdg_teamChatChattingBox.addItem(f"[{message_datetime}] [{sender_name}]\n{message}")
+        self.led_teamChatSendMessage.clear()  # 작성한 글은 전송 후 ui에서 지워줌
+
+        # 리스트 위젯 스크롤바 아래로 고정
+        self.listwdg_teamChatChattingBox.scrollToBottom()
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    myWindow = WindowClass()
+    myWindow = Client()
     myWindow.show()
     app.exec_()
--- chat_server_br.py
+++ chat_server_br.py
@@ -1,89 +1,217 @@
+# TCP 멀티 채팅 서버 프로그램
+
 from socket import *
 from threading import *
+from datetime import timedelta, datetime, time
 import pymysql
 import json
-import datetime
-from datetime import timedelta, datetime, time
 
 
 class MultiChatServer:
 
+    # 소켓을 생성하고 연결되면 accept_client() 호출
     def __init__(self):
-        self.clients = []
-        self.final_received_message = ""
-        self.s_sock = socket(AF_INET, SOCK_STREAM)
-        self.ip = '127.0.0.1'
-        self.port = 5959
+        self.clients = []                   # 접속된 클라이언트 소켓 목록을 넣을 리스트
+        self.final_recived_message = ""     # 수신 메시지를 문자열로 저장할 빈 문자열
+
+        self.s_sock = socket(AF_INET, SOCK_STREAM)      # 서버 소켓 생성
+        self.ip = '10.10.21.118'
+        self.port = 6666
         self.s_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         self.s_sock.bind((self.ip, self.port))
-        print('클라이언트 대기중. . . ')
-        self.s_sock.listen(3)
+        print("클라이언트 접속 대기 중...")
+        self.s_sock.listen(2)
         self.accept_client()
+        self.connectedMember = []       # 현재 접속한 사람 리스트
 
+    def open_db(self):
+        self.conn = pymysql.connect(host='10.10.21.102', user='lilac', password='0000', db='network_project', charset='utf8')
+        self.c = self.conn.cursor()
+    # 모든 채팅 DB에서 가져오기
+    def method_getAllChat(self):
+        self.open_db()
+        # 채팅로그 불러오기
+        self.c.execute("SELECT * FROM chat")
+        chat_info = self.c.fetchall()
+        self.conn.close()
+
+        list_chat_info = ['allChat_data']
+
+        # DB에서 가져온 튜플 리스트화
+        for i in range(len(chat_info)):
+            for j in range(len(chat_info[i])):
+                if type(chat_info[i][j]) == datetime:
+                    list_chat_info.append(chat_info[i][j].strftime('%D %T'))
+                else:
+                    list_chat_info.append(chat_info[i][j])
+
+        return list_chat_info
+
+    # 모든 접속 멤버 가져오기
+    def method_getAllConnection(self):
+        self.open_db()
+        # 접속멤버 불러오기
+        self.c.execute("SELECT connection_person FROM connection WHERE connection_stat = '입장'")
+        connection_data = self.c.fetchall()
+        self.conn.close()
+
+        list_connection_info = ['allConnection_data']
+
+        # DB에서 가져온 튜플 리스트화
+        for i in range(len(connection_data)):
+            for j in range(len(connection_data[i])):
+                list_connection_info.append(connection_data[i][j])
+
+        return list_connection_info
+
+    def method_getAllNewChat(self):
+        self.open_db()
+        # 채팅로그 불러오기
+        self.c.execute("select chatlist from network_project.new_chat;")
+        Newchat_list = self.c.fetchall()
+        self.conn.close()
+
+        list_Newchat_info = ['allNewChat_data']
+
+        # DB에서 가져온 튜플 리스트화
+        for i in range(len(Newchat_list)):
+            for j in range(len(Newchat_list[i])):
+                list_Newchat_info.append(Newchat_list[i][j])
+
+        return list_Newchat_info
+
+
+    # 연결 클라이언트 소켓을 목록에 추가하고 스레드를 생성하여 데이터를 수신한다
     def accept_client(self):
         while True:
             client = c_socket, (ip, port) = self.s_sock.accept()
             if client not in self.clients:
-                self.clients.append(client)
-                self.open_db()
-                self.c.execute('SELECT * FROM network_project.chat')
-                chat_list = self.c.fetchall()
+                self.clients.append(client)  # 접속된 소켓을 목록에 추가
 
-                # DB에서 가져온 데이터를 튜플리스트화
-                list_chat_info = []
-                for i in range(len(chat_list)):
-                    for j in range(len(chat_list[i])):
-                        if type(chat_list[i][j]) == datetime:
-                            list_chat_info.append(chat_list[i][j].strftime('%D %T'))
-                        else:
-                            list_chat_info.append(chat_list[i][j])
+                list_chat_info = self.method_getAllChat()   # 모든 채팅 DB에서 가져오기
+                setdata = json.dumps(list_chat_info)        # json.dumps로 리스트의 값들 바이트형으로 바꿔줌
+                c_socket.send(setdata.encode())             # 연결된 소켓에 채팅 로그 데이터 보내줌
 
-                setdata = json.dumps(list_chat_info)
-                c_socket.send(setdata.encode())
+                list_connection_info = self.method_getAllConnection()   # 모든 접속자 DB에서 가져오기
+                setdata = json.dumps(list_connection_info)              # json.dumps로 리스트의 값들 바이트형으로 바꿔줌
+                c_socket.send(setdata.encode())                         # 연결된 소켓에 채팅 로그 데이터 보내줌
 
-                self.conn.close()
+                # list_Newchat_info = self.method_getAllNewChat()
+                # setdata = json.dumps(list_Newchat_info)
+                # c_socket.send(setdata.encode())
 
-                # print(list_chat_info)
-            print(f'{ip},:,{str(port)}가 접속하었습니다.')
-            cth = Thread(target=self.receive_messages, args=(c_socket,),daemon=True)
-            cth.start()
+            print(datetime.now().strftime('%D %T'), '주소:', ip, ' 포트번호:', str(port), '가 연결되었습니다')
 
-    def open_db(self):
-        self.conn = pymysql.connect(host='10.10.21.102', user='lilac', password='0000', db='network_project',charset='utf8')
-        self.c = self.conn.cursor()
+            ctn = Thread(target=self.receive_messages, args=(c_socket,), daemon=True)    # 수신 스레드
+            ctn.start()     # 스레드 시작
 
-    # 데이터를 수신하여 접속한 모든 클라이언트에게 채팅
+    # 데이터를 수신하여 모든 클라이언트에게 전송하고 수신한 데이터를 DB에 저장한다
     def receive_messages(self, c_socket):
         while True:
             try:
-                incoming_message = c_socket.recv(256)
-                if not incoming_message:
+                incoming_message = c_socket.recv(9999)
+                if not incoming_message:    # 연결이 종료됨
                     break
             except:
                 continue
             else:
-                self.final_received_message = incoming_message.decode('utf-8')
-                self.send_all_clients(c_socket)
+                self.recived_message = json.loads(incoming_message.decode('utf-8'))
+                print(self.recived_message)
+                if self.recived_message[0] == 'plzReceiveMessage':
+                    self.sendMessage_all_clients(c_socket)      # 열려있는 모든 클라이언트들에게 메세지 보내기
+                if self.recived_message[0] == 'plzReceiveAlarm':
+                    self.sendAlarm_all_clients(c_socket)        # 열려있는 모든 클라이언트들에게 알림 보내기
+                if self.recived_message[0] == 'plzReceiveNewchat':
+                    self.sendNewchat_all_clients(c_socket)        # 열려있는 모든 클라이언트들에게 새채팅방 보내기
+                if self.recived_message[0] == 'plzReceiveNewMessage':
+                    self.sendNewchat_all_clients(c_socket)          # 열려있는 모든 클라이언트들에게 새로운메세지 보내기
 
-                print(self.final_received_message)
-
-                self.open_db()
-                self.c.execute (f"INSERT INTO testchat VALUES (now(), '{self.final_received_message[1]}', '{self.final_received_message[2]}')")
-                self.conn.commit()
 
         c_socket.close()
 
-    def send_all_clients(self, senders_socket):
-        sendall_message = f"[{self.final_received_message[0]}] [{self.final_received_message[1]}]\n{self.final_received_message[2]}"
-        for client in self.clients:
+    # 모든 클라이언트로 메시지 보내기
+    def sendMessage_all_clients(self, senders_socket):
+        message = ['plzReceiveMessage',
+                   f"[{self.recived_message[1]}] [{self.recived_message[2]}]\n{self.recived_message[3]}"]
+        sendall_message = json.dumps(message)
+        for client in self.clients:     # 목록에 있는 모든 소켓에 대해
             socket, (ip, port) = client
-            if socket is not senders_socket:
+            if socket is not senders_socket:    # 송신 클라이언트는 제외
                 try:
                     socket.sendall(sendall_message.encode())
-                except:
-                    self.clients.remove(client)
-                    print("{},{} 연결이 종료되었습니다".format(ip, port))
+                except:     # 메시지가 전송되지 않으면 연결 종료된 소켓이므로 지워준다
+                    self.clients.remove(client)     # 소켓 제거
+                    print(f"{datetime.now().strftime('%D %T')}, {ip}, {port} 연결이 종료되었습니다")
+
+        self.open_db()
+        # insert문 넣어주기(언제몇시몇분에 누가 채팅을 쳤습니다)
+        self.c.execute(f"INSERT INTO chat VALUES (now(), '{self.recived_message[2]}', '{self.recived_message[3]}')")
+        self.conn.commit()
+        self.conn.close()
+
+
+    # 모든 클라이언트로 알람 보내기
+    def sendAlarm_all_clients(self, senders_socket):
+        alarm = ['plzReceiveAlarm',
+                 f"\n<<< [{self.recived_message[1]}] [{self.recived_message[2]}] 님이 채팅방에 입장하셨습니다 >>>\n",
+                 self.recived_message[2]]
+        sendall_Alarm = json.dumps(alarm)
+        for client in self.clients:  # 목록에 있는 모든 소켓에 대해
+            socket, (ip, port) = client
+            if socket is not senders_socket:  # 송신 클라이언트는 제외
+                try:
+                    socket.sendall(sendall_Alarm.encode())      # 연결된 소켓(클라이언트)에 알람 데이터 보내줌
+                except:  # 연결 종료
+                    self.clients.remove(client)  # 소켓 제거
+                    print(f"{datetime.now().strftime('%D %T')}, {ip}, {port} 연결이 종료되었습니다")
+
+        self.open_db()
+        # insert문 넣어주기(언제몇시몇분에 누가 입장했습니다)
+        self.c.execute(f"INSERT INTO connection VALUES (now(), '{self.recived_message[2]}', '입장')")
+        self.conn.commit()
+        self.conn.close()
+
+    # 모든 클라이언트에게 새채팅방 보내기
+    def sendNewchat_all_clients(self, senders_socket):
+        message = ['plzReceiveNewchat',f"[{self.recived_message[1]}]"]
+        print(message)
+        sendall_Newchat = json.dumps(message)
+        for client in self.clients:  # 목록에 있는 모든 소켓에 대해
+            socket, (ip, port) = client
+            if socket is not senders_socket:  # 송신 클라이언트는 제외
+                try:
+                    socket.sendall(sendall_Newchat.encode())
+                except:  # 메시지가 전송되지 않으면 연결 종료된 소켓이므로 지워준다
+                    self.clients.remove(client)  # 소켓 제거
+                    print(f"{datetime.now().strftime('%D %T')}, {ip}, {port} 연결이 종료되었습니다")
+
+        self.open_db()
+        # insert문 넣어주기(새로운 채팅방 저장)
+        self.c.execute(f"INSERT INTO new_chat (chatlist) VALUES ('{self.recived_message[1]}')")
+        self.conn.commit()
+        self.conn.close()
+
+    def sendNewMessage_all_clients(self, senders_socket):
+        newmessage = ['plzReceiveNewMessage',
+                   f"[{self.recived_message[1]}] [{self.recived_message[2]}]\n{self.recived_message[3]}"]
+        print(newmessage)
+        sendall_newmessage = json.dumps(newmessage)
+        for client in self.clients:     # 목록에 있는 모든 소켓에 대해
+            socket, (ip, port) = client
+            if socket is not senders_socket:    # 송신 클라이언트는 제외
+                try:
+                    socket.sendall(sendall_newmessage.encode())
+                except:     # 메시지가 전송되지 않으면 연결 종료된 소켓이므로 지워준다
+                    self.clients.remove(client)     # 소켓 제거
+                    print(f"{datetime.now().strftime('%D %T')}, {ip}, {port} 연결이 종료되었습니다")
+
+        self.open_db()
+        # insert문 넣어주기(언제몇시몇분에 누가 채팅을 쳤습니다)
+        self.c.execute(f"insert into network_project.new_chat (chatlist,name,message) value ({self.recived_message[1]},'{self.recived_message[2]}','{self.recived_message[3]}')")
+        self.conn.commit()
+        self.conn.close()
 
 
 if __name__ == "__main__":
     MultiChatServer()
+
